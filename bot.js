390c69bb718fd3cd2f034d6adc07cf6a387364d9
FutureXpo <32868033+FutureXpo@users.noreply.github.com>
2017-10-17 21:32:23 +0300

    Update bot.js
    Исправлена табуляция
    Ссылка на добавление на другой сервер отправляется в лс

--- bot.js
+++ bot.js
@@ -8,35 +8,35 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    /*Базовая команда*/
-    if (message.content === 'ping') {
-    	message.reply('pong')
-    }
+	/*Базовая команда*/
+	if (message.content === 'ping') {
+		message.channel.send('pong')
+	}
 	
-    /*Не отвечает, если автор сообщения - другой бот*/
-    if(message.author.bot) return;
+	/*Не отвечает, если автор сообщения - другой бот*/
+	if(message.author.bot) return;
 	
-    /*Не отвечает, если в начале сообщения не стоит знак обращения*/
-    if(message.content.indexOf(process.env.PREFIX) !== 0) return;
+	/*Не отвечает, если в начале сообщения не стоит знак 'обращения'*/
+	if(message.content.indexOf(process.env.PREFIX) !== 0) return;
 	
-    /*Получает данные команды*/
+	/*Получает данные команды*/
 	const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
 	const command = args.shift().toLowerCase();
-    
-    /*Возвращает пинг бота*/
-    if (command === 'ping'||command === "пинг") {
-		message.reply(`Пинг - ${Math.round(client.ping)}ms`);
+	
+	/*Возвращает пинг бота*/
+	if (command === 'ping'||command === "пинг") {
+		message.reply(`Пинг моего сервера - ${Math.round(client.ping)}мс`);
 	}
     
-    /*Говорит с помощью tts фразу*/
-    if(command === "say"||command === "скажи") {
+	/*Говорит с помощью tts фразу*/
+	if(command === "say"||command === "скажи") {
 		const sayMessage = args.join(" ");
 		message.delete().catch(O_o=>{});  
 		message.channel.send(sayMessage,{tts:true});
 	}
-    
-    /*Очищает до 100 сообщений(включая сообщение с командой)*/
-    if(command === "clear"||command === "clean"||command === "очистить"||command === "очисти") {
+	
+	/*Очищает до 100 сообщений(включая сообщение с командой)*/
+	if(command === "clear"||command === "clean"||command === "очистить"||command === "очисти") {
 		if (message.content===process.env.PREFIX+command)
 			return message.channel.fetchMessages({limit: 100})
 				.then(messages => messages.deleteAll())
@@ -48,10 +48,10 @@ client.on('message', message => {
 			.then(messages => messages.deleteAll())
 			.catch(console.error);
 	}
-    
-    /*Возвращает справку*/
-    if(command === "help"||command === "помощь"||command === "справка") {
-	message.reply(`
+	
+	/*Возвращает справку*/
+	if(command === "help"||command === "помощь"||command === "справка") {
+		return message.reply(`
 **__Справка обо мне__**:
 	Обращаться со знаком "`+process.env.PREFIX+`", другие сообщения не читаю
 **__Знаю команды__**:
@@ -61,17 +61,18 @@ client.on('message', message => {
 	invite - пригласить меня в чат`);
 	}
     
-    /*Отправляет ссылку для добавления бота в другой чат*/
-    if (command === 'invite'||command === "пригласить") {
-	message.reply(`Ссылка для добавления меня в чат: https://discordapp.com/oauth2/authorize?&client_id=` + process.env.BOT_ID + `&scope=bot&permissions=8`);
-    }
+	/*Отправляет ссылку для добавления бота в другой чат*/
+	if (command === 'invite'||command === "пригласить") {
+		message.author.send(`Ссылка для добавления меня в чат: https://discordapp.com/oauth2/authorize?&client_id=` + process.env.BOT_ID + `&scope=bot&permissions=8`);
+		message.delete().catch(O_o=>{});  
+	}
 });
 
 /*Пишет в чат о том, что человек покинул сервер*/
 client.on('guildMemberAdd', member => {
-  	const channel = member.guild.channels.find('name', 'member-log');
-  	if (!channel) return;
-  	channel.send(`К великому сожалению, нас покинул холоп ${member}`);
+	const channel = member.guild.channels.find('name', 'member-log');
+	if (!channel) return;
+	channel.send(`К великому сожалению, нас покинул холоп ${member}((`);
 });
 
 /*Пишет в лог, когда бота добавили насервер*/
