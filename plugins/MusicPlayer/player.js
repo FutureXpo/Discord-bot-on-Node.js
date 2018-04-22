3de300630341aff94afbaa760866de79283385bf
FutureX <dimal2000futurex2000@gmail.com>
2018-04-22 12:27:03 +0300

    1

--- plugins/MusicPlayer/player.js
+++ plugins/MusicPlayer/player.js
@@ -1,9 +1,115 @@
 const yt = require('ytdl-core');
+var request1 = require('superagent');
 
 const API_KEY = process.env.YOUTUBE_API_KEY;
 const WATCH_VIDEO_URL = "https://www.youtube.com/watch?v=";
 
 let queue = {};
+
+const commands = {
+	'play_': (msg) => {
+		if (queue[msg.guild.id] === undefined) return msg.channel.sendMessage(`Добавьте больше песен в список с помощью команды \'${process.env.PREFIX}play\'`);
+		if (!msg.guild.voiceConnection) return commands.join(msg).then(() => commands.play_(msg));
+		if (queue[msg.guild.id].playing) return /*msg.channel.sendMessage('Уже работаю...')*/;
+		let dispatcher;
+		queue[msg.guild.id].playing = true;
+
+	//	console.log(queue);
+		(function play(song) {
+	//		console.log(song);
+			if (song === undefined) return msg.channel.sendMessage('Список воспроизведения пуст').then(() => {
+				queue[msg.guild.id].playing = false;
+				msg.member.voiceChannel.leave();
+			});
+			msg.channel.sendMessage(`Играет: ** ${song.title} **`);
+			dispatcher = msg.guild.voiceConnection.playStream(yt(song.url, { audioonly: true }), { passes : process.env.passes });
+			let collector = msg.channel.createCollector(m => m);
+			collector.on('message', m => {
+			/*	if (m.content.startsWith(process.env.PREFIX + 'pause')) {
+					msg.channel.sendMessage(`Пауза`).then(() => {dispatcher.pause();});
+				} else*/ if (m.content.startsWith(process.env.PREFIX + 'play')){
+					dispatcher.resume();
+				} else if (m.content.startsWith(process.env.PREFIX + 'skip')){
+					msg.channel.sendMessage(`~~**$Пропущена песня**~~`).then(() => {dispatcher.end();});
+				} else if (m.content.startsWith(process.env.PREFIX + 'vol+')){
+					if (Math.round(dispatcher.volume*50) >= 100) return msg.channel.sendMessage(`Volume: ${Math.round(dispatcher.volume*50)}%`);
+					dispatcher.setVolume(Math.min((dispatcher.volume*50 + (2*(m.content.split('+').length-1)))/50,2));
+					msg.channel.sendMessage(`Громкость: ${Math.round(dispatcher.volume*50)}%`);
+				} else if (m.content.startsWith(process.env.PREFIX + 'vol-')){
+					if (Math.round(dispatcher.volume*50) <= 0) return msg.channel.sendMessage(`Volume: ${Math.round(dispatcher.volume*50)}%`);
+					dispatcher.setVolume(Math.max((dispatcher.volume*50 - (2*(m.content.split('-').length-1)))/50,0));
+					msg.channel.sendMessage(`Громкость: ${Math.round(dispatcher.volume*50)}%`);
+				} else if (m.content.startsWith(process.env.PREFIX + 'time')){
+					msg.channel.sendMessage(`Играет: ${Math.floor(dispatcher.time / 60000)}:${Math.floor((dispatcher.time % 60000)/1000) <10 ? '0'+Math.floor((dispatcher.time % 60000)/1000) : Math.floor((dispatcher.time % 60000)/1000)}`);
+				}
+			});
+			dispatcher.on('end', () => {
+				collector.stop();
+				play(queue[msg.guild.id].songs.shift());
+			});
+			dispatcher.on('error', (err) => {
+				return msg.channel.sendMessage('Ошибка: ' + err).then(() => {
+					collector.stop();
+					play(queue[msg.guild.id].songs.shift());
+				});
+			});
+		})(queue[msg.guild.id].songs.shift());
+	},
+	'join': (msg) => {
+		return new Promise((resolve, reject) => {
+			const voiceChannel = msg.member.voiceChannel;
+			if (!voiceChannel || voiceChannel.type !== 'voice') return msg.reply('Не могу подключиться к голосовому чату...');
+			voiceChannel.join().then(connection => resolve(connection)).catch(err => reject(err));
+		});
+	},
+	'play': (msg) => {
+		let arg = msg.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
+		arg.shift().toLowerCase();
+		let url = arg.join('%20');
+		if (url == '' || url === undefined) return msg.channel.sendMessage(`Необходимо добавить песен в список воспроизведения`);
+		yt.getInfo(url, (err, info) => {
+			if(err) {	
+			//	url = url.split(' ').join('%20');
+				console.log('https://www.googleapis.com/youtube/v3/search' + '?part=snippet&q=' + url + '&key=' + API_KEY);
+				var requestUrl = 'https://www.googleapis.com/youtube/v3/search' + '?part=snippet&q=' + url + '&key=' + API_KEY;
+				requestl(requestUrl, (error, response) => {
+					if (!error && response.statusCode == 200) {
+						var body = response.body;
+					//	console.log(body);
+						if (body.items.length == 0) {
+							console.log("I Could Not Find Anything!");
+							msg.channel.sendMessage('Invalid YouTube Link: ' + err);
+							return;
+						}
+						for (var item of body.items) {
+							if (item.id.kind == 'youtube#video') {
+								url = WATCH_VIDEO_URL+item.id.videoId;
+								var name = item.id.title;
+								if (!queue.hasOwnProperty(msg.guild.id)) queue[msg.guild.id] = {}, queue[msg.guild.id].playing = false, queue[msg.guild.id].songs = [];
+								queue[msg.guild.id].songs.push({url: url, title: name, requester: msg.author.username});
+								msg.channel.sendMessage(`**${item.id.title}** __теперь в текущем плейлисте__`).then(() => commands.play_(msg));
+								return;
+							}
+						}
+					} else {
+						console.log("Unexpected error!");
+						return;
+					}
+				});	
+				return;
+			}
+			if (!queue.hasOwnProperty(msg.guild.id)) queue[msg.guild.id] = {}, queue[msg.guild.id].playing = false, queue[msg.guild.id].songs = [];
+			queue[msg.guild.id].songs.push({url: url, title: info.title, requester: msg.author.username});
+			msg.channel.sendMessage(`**${info.title}** __теперь в текущем плейлисте__`).then(() => commands.play_(msg));
+		});
+	},
+        'queue': (msg) => {
+		if (queue[msg.guild.id] === undefined) return msg.channel.sendMessage(`Добавьте больше песен в список с помощью команды \'${process.env.PREFIX}play\'`);
+		let tosend = [];
+		queue[msg.guild.id].songs.forEach((song, i) => { tosend.push(`${i+1}. ${song.title} - Добавил : ${song.requester}`);});
+		msg.channel.sendMessage(`__**Плейлист:**__ Песен в очереди - **${tosend.length}** ${(tosend.length > 7 ? '*[Показаны только следующие 7 песен]*' : '')}\`\`\`\n ${tosend.slice(0,7).join('\n')} \`\`\``);
+	}
+};
 
 exports.commands = [
 	"play",
@@ -109,112 +215,6 @@ exports.volume = {
 		}
 	}
 }
-
-
-const commands = {
-	'play_': (msg) => {
-		if (queue[msg.guild.id] === undefined) return msg.channel.sendMessage(`Добавьте больше песен в список с помощью команды \'${process.env.PREFIX}play\'`);
-		if (!msg.guild.voiceConnection) return commands.join(msg).then(() => commands.play_(msg));
-		if (queue[msg.guild.id].playing) return /*msg.channel.sendMessage('Уже работаю...')*/;
-		let dispatcher;
-		queue[msg.guild.id].playing = true;
-
-	//	console.log(queue);
-		(function play(song) {
-	//		console.log(song);
-			if (song === undefined) return msg.channel.sendMessage('Список воспроизведения пуст').then(() => {
-				queue[msg.guild.id].playing = false;
-				msg.member.voiceChannel.leave();
-			});
-			msg.channel.sendMessage(`Играет: ** ${song.title} **`);
-			dispatcher = msg.guild.voiceConnection.playStream(yt(song.url, { audioonly: true }), { passes : process.env.passes });
-			let collector = msg.channel.createCollector(m => m);
-			collector.on('message', m => {
-			/*	if (m.content.startsWith(process.env.PREFIX + 'pause')) {
-					msg.channel.sendMessage(`Пауза`).then(() => {dispatcher.pause();});
-				} else*/ if (m.content.startsWith(process.env.PREFIX + 'play')){
-					dispatcher.resume();
-				} else if (m.content.startsWith(process.env.PREFIX + 'skip')){
-					msg.channel.sendMessage(`~~**$Пропущена песня**~~`).then(() => {dispatcher.end();});
-				} else if (m.content.startsWith(process.env.PREFIX + 'vol+')){
-					if (Math.round(dispatcher.volume*50) >= 100) return msg.channel.sendMessage(`Volume: ${Math.round(dispatcher.volume*50)}%`);
-					dispatcher.setVolume(Math.min((dispatcher.volume*50 + (2*(m.content.split('+').length-1)))/50,2));
-					msg.channel.sendMessage(`Громкость: ${Math.round(dispatcher.volume*50)}%`);
-				} else if (m.content.startsWith(process.env.PREFIX + 'vol-')){
-					if (Math.round(dispatcher.volume*50) <= 0) return msg.channel.sendMessage(`Volume: ${Math.round(dispatcher.volume*50)}%`);
-					dispatcher.setVolume(Math.max((dispatcher.volume*50 - (2*(m.content.split('-').length-1)))/50,0));
-					msg.channel.sendMessage(`Громкость: ${Math.round(dispatcher.volume*50)}%`);
-				} else if (m.content.startsWith(process.env.PREFIX + 'time')){
-					msg.channel.sendMessage(`Играет: ${Math.floor(dispatcher.time / 60000)}:${Math.floor((dispatcher.time % 60000)/1000) <10 ? '0'+Math.floor((dispatcher.time % 60000)/1000) : Math.floor((dispatcher.time % 60000)/1000)}`);
-				}
-			});
-			dispatcher.on('end', () => {
-				collector.stop();
-				play(queue[msg.guild.id].songs.shift());
-			});
-			dispatcher.on('error', (err) => {
-				return msg.channel.sendMessage('Ошибка: ' + err).then(() => {
-					collector.stop();
-					play(queue[msg.guild.id].songs.shift());
-				});
-			});
-		})(queue[msg.guild.id].songs.shift());
-	},
-	'join': (msg) => {
-		return new Promise((resolve, reject) => {
-			const voiceChannel = msg.member.voiceChannel;
-			if (!voiceChannel || voiceChannel.type !== 'voice') return msg.reply('Не могу подключиться к голосовому чату...');
-			voiceChannel.join().then(connection => resolve(connection)).catch(err => reject(err));
-		});
-	},
-	'play': (msg) => {
-		let arg = msg.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
-		arg.shift().toLowerCase();
-		let url = arg.join('%20');
-		if (url == '' || url === undefined) return msg.channel.sendMessage(`You must add a YouTube video url, or id after ${process.env.PREFIX}p`);
-		yt.getInfo(url, (err, info) => {
-			if(err) {	
-			//	url = url.split(' ').join('%20');
-				console.log('https://www.googleapis.com/youtube/v3/search' + '?part=snippet&q=' + url + '&key=' + API_KEY);
-				var requestUrl = 'https://www.googleapis.com/youtube/v3/search' + '?part=snippet&q=' + url + '&key=' + API_KEY;
-				request1(requestUrl, (error, response) => {
-					if (!error && response.statusCode == 200) {
-						var body = response.body;
-					//	console.log(body);
-						if (body.items.length == 0) {
-							console.log("I Could Not Find Anything!");
-							msg.channel.sendMessage('Invalid YouTube Link: ' + err);
-							return;
-						}
-						for (var item of body.items) {
-							if (item.id.kind == 'youtube#video') {
-								url = WATCH_VIDEO_URL+item.id.videoId;
-								var name = item.id.title;
-								if (!queue.hasOwnProperty(msg.guild.id)) queue[msg.guild.id] = {}, queue[msg.guild.id].playing = false, queue[msg.guild.id].songs = [];
-								queue[msg.guild.id].songs.push({url: url, title: name, requester: msg.author.username});
-								msg.channel.sendMessage(`**${item.id.title}** __теперь в текущем плейлисте__`).then(() => commands.play_(msg));
-								return;
-							}
-						}
-					} else {
-						console.log("Unexpected error!");
-						return;
-					}
-				});	
-				return;
-			}
-			if (!queue.hasOwnProperty(msg.guild.id)) queue[msg.guild.id] = {}, queue[msg.guild.id].playing = false, queue[msg.guild.id].songs = [];
-			queue[msg.guild.id].songs.push({url: url, title: info.title, requester: msg.author.username});
-			msg.channel.sendMessage(`**${info.title}** __теперь в текущем плейлисте__`).then(() => commands.play_(msg));
-		});
-	},
-        'queue': (msg) => {
-		if (queue[msg.guild.id] === undefined) return msg.channel.sendMessage(`Добавьте больше песен в список с помощью команды \'${process.env.PREFIX}play\'`);
-		let tosend = [];
-		queue[msg.guild.id].songs.forEach((song, i) => { tosend.push(`${i+1}. ${song.title} - Добавил : ${song.requester}`);});
-		msg.channel.sendMessage(`__**Плейлист:**__ Песен в очереди - **${tosend.length}** ${(tosend.length > 7 ? '*[Показаны только следующие 7 песен]*' : '')}\`\`\`\n ${tosend.slice(0,7).join('\n')} \`\`\``);
-	}
-};
 
 function getAuthorVoiceChannel(msg) {
 	var voiceChannelArray = msg.guild.channels.filter((v)=>v.type == "voice").filter((v)=>v.members.has(msg.author.id)).array();
